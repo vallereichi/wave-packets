dc82a9ece1ef86951038138b72792d8845468f81
Valentin Reichenspurner <vreichenspurner@tum.de>
2024-06-07 13:20:23 +0200

    added better build system for macos

---
+++ build/src/CMakeFiles/renderClasses.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/renderClasses.dir/renderer.cpp.o"
+  "CMakeFiles/renderClasses.dir/renderer.cpp.o.d"
+  "CMakeFiles/renderClasses.dir/shader.cpp.o"
+  "CMakeFiles/renderClasses.dir/shader.cpp.o.d"
+  "CMakeFiles/renderClasses.dir/vertexArray.cpp.o"
+  "CMakeFiles/renderClasses.dir/vertexArray.cpp.o.d"
+  "CMakeFiles/renderClasses.dir/vertexBuffer.cpp.o"
+  "CMakeFiles/renderClasses.dir/vertexBuffer.cpp.o.d"
+  "librenderClasses.a"
+  "librenderClasses.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/renderClasses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
